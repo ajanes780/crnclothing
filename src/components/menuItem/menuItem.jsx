8b0dcb425e9bf168de66bba7154ad17488a6f06e
Aaron Janes <Aaron.Janes@gmail.com>
2021-03-29 11:57:46 -0600

    add transfrom to classes

--- src/components/menuItem/menuItem.jsx
+++ src/components/menuItem/menuItem.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./menuItemStyle.scss";
 
 export const MenuitemComponent = ({ title, imageUrl, size }) => (
-  <div
-    style={{ "background-image": `url(${imageUrl})` }}
-    className={`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      style={{ "background-image": `url(${imageUrl})` }}
+      className="background-image"
+    />
     <div style={{ "background-color": "#fff" }} className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
