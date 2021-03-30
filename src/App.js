d7fd1bd759c95278fcb005ccd0766cd22444e53c
Aaron Janes <Aaron.Janes@gmail.com>
2021-03-30 10:56:46 -0600

    Build Header Component and Style it

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import { HomePage } from "./pages/homepage/HomepageComponent";
 import { ShopComponent } from "./pages/shop/shopComponent";
-import { Switch, Route, link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import { HeaderComponent } from "./components/HeaderComponent/HeaderComponent";
 
 function App() {
   return (
     <div className="App">
+      <HeaderComponent />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopComponent} />
