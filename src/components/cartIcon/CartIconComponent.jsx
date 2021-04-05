8c92946bc6be902ee196f2cb11184b40145ee3c4
Aaron Janes <Aaron.Janes@gmail.com>
2021-04-04 20:22:41 -0600

    build reducer to count total items in cart, Map State to props to passc count to cart icon  working code

--- src/components/cartIcon/CartIconComponent.jsx
+++ src/components/cartIcon/CartIconComponent.jsx
@@ -5,10 +5,10 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartHidden } from "../../redux/cart/cartActions";
 
 import "./cartIconComponentStyle.scss";
-const CartIconComponent = ({ toggleCartHidden }) => (
+const CartIconComponent = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -16,4 +16,11 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIconComponent);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIconComponent);
